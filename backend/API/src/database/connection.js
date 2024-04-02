9139a36173365e241b449b3083275e2a2ac1b9e6
Anderson <loonander@gmail.com>
2024-04-02 15:42:39 -0300

    changes

--- backend/API/src/database/connection.js
+++ backend/API/src/database/connection.js
@@ -9,13 +9,14 @@ const dbUser= process.env.DB_USERNAME
 const dbPassword = process.env.DB_PASSWORD
 const database = process.env.DATABASE
 const dbHost = process.env.DB_HOST
-const dbDialect= process.env.DB_DIALECT
+const dbPort = process.env.DB_PORT
 
 
 
 
 export const sequelize = new Sequelize(database, dbUser, dbPassword,{
     host: dbHost,
+    port: dbPort,
     dialect: 'mysql'
 
 })
